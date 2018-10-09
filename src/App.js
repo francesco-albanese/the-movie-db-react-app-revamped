5532a6f6b09a1888cb9fe2ef1cdb52b3f1efcecb
Francesco Albanese <francescoalbanese@Francescos-MacBook-Pro.local>
2018-10-09 11:52:44 +0200

    Clean App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src="./logo.svg" className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <div>Test</div>
+    )
   }
 }
 
-export default App;
+export default App
