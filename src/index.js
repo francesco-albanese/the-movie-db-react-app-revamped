0e334a40b1a80221aeb94f89c61a1d4a157ac44c
Francesco Albanese <francescoalbanese@Francescos-MacBook-Pro.local>
2018-10-09 11:53:35 +0200

    Cleanup of unused files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import { CssBaseline } from '@material-ui/core'
+import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import './main.scss'
+
+const app = (
+  <React.Fragment>
+    <CssBaseline />
+    <App />
+  </React.Fragment>
+)
+
+ReactDOM.render(app, document.getElementById('root'))
+registerServiceWorker()
