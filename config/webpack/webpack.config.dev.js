a1b1f053f7a105fbdfc4ad19d7791eb932bcc344
Francesco Albanese <francescoalbanese@Francescos-MacBook-Pro.local>
2018-10-09 11:52:10 +0200

    Add sass globImporter to webpack config

--- config/webpack/webpack.config.dev.js
+++ config/webpack/webpack.config.dev.js
@@ -1,5 +1,6 @@
 
 const autoprefixer = require('autoprefixer');
+const globImporter = require('node-sass-glob-importer')
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
@@ -156,7 +157,7 @@ module.exports = {
           // In production, we use a plugin to extract that CSS to a file, but
           // in development "style" loader enables hot editing of CSS.
           {
-            test: /\.css$/,
+            test: /\.(css|scss)$/,
             use: [
               require.resolve('style-loader'),
               {
@@ -185,6 +186,12 @@ module.exports = {
                   ],
                 },
               },
+              {
+                loader: require.resolve('sass-loader'),
+                options: {
+                  importer: globImporter()
+                }
+              }
             ],
           },
           // "file" loader makes sure those assets get served by WebpackDevServer.
