a60264bed41b2361f6d41bac6971b70655b3a00b
Francesco Albanese <francescoalbanese@Francescos-MacBook-Pro.local>
2018-10-15 20:59:03 +0200

    Fetch all locales from Contentful

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react'
 import withWidth from '@material-ui/core/withWidth'
+import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
+import { isEmpty } from 'lodash-es'
 
 import { decorateClass, getIsMobile } from '#utils'
 
+import { fetchAllLocales, getAllLocales } from '@themoviedb/the-movie-db-store'
+
 class App extends Component {
+
+  componentDidMount() {
+    const { allLocales, fetchAllLocales } = this.props
+
+    if (isEmpty(allLocales)) {
+      fetchAllLocales()
+    }
+
+  }
+
   render() {
     const { width } = this.props
     const isMobile = getIsMobile(width)
@@ -17,7 +31,15 @@ class App extends Component {
   }
 }
 
+const mapStateToProps = state => ({
+  allLocales: getAllLocales(state)
+})
+
+const mapDispatchToProps = dispatch => bindActionCreators({
+  fetchAllLocales
+}, dispatch)
+
 export default decorateClass([
   withWidth({ withTheme: true }),
-  connect()
+  connect(mapStateToProps, mapDispatchToProps)
 ], App)
