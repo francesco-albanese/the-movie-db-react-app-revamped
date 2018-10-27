f38e3c65e8f913f8430456f8a86e8c39c1e78018
Francesco Albanese <francescoalbanese@Francescos-MacBook-Pro.local>
2018-10-27 15:04:16 +0200

    Create RootPage function as children

--- src/functions-as-children/RootPage/index.js
+++ src/functions-as-children/RootPage/index.js
@@ -3,6 +3,7 @@ import withWidth from '@material-ui/core/withWidth'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
+import { isEmpty } from 'lodash-es'
 import PropTypes from 'prop-types'
 
 import { decorateClass, getIsMobile } from '#utils'
@@ -10,31 +11,84 @@ import { decorateClass, getIsMobile } from '#utils'
 import {
   getAllPages,
   getActiveLocale,
+  getActivePage,
   setActivePage
 } from '@themoviedb/the-movie-db-store'
 
 class RootPage extends React.Component {
 
-  state = {
-    
-  }
-
   static propTypes = {
     children: PropTypes.func.isRequired
   }
 
+  state = {
+
+  }
+
+  get404page = () => {
+    const { allPages } = this.props
+
+    const page404 = allPages.find(page => page.reference.includes('four-oh-four-page'))
+
+    return page404
+  }
+
+  componentDidMount() {
+    const { 
+      activeLocale,
+      allPages,
+      history, 
+      location, 
+      match,
+      setActivePage 
+    } = this.props
+
+    if (location.pathname === '/') {
+      /**
+       * this is to prevent 404 page
+       * from being set by default
+       * if url is visited without
+       * valid locale (en|it)
+       */
+      history.push(activeLocale.path)
+    }
+
+    const pagePath = isEmpty(match.params) 
+      ? location.pathname
+      : match.path
+
+    const page = allPages.find(page => page.paths[ activeLocale.code ] === pagePath)
+
+    if (page) {
+      setActivePage(page)
+    } else {
+      setActivePage(this.get404page())
+    }
+  }
+
+
   render() {
-    const { children, width } = this.props
+    const { 
+      activePage, 
+      children, 
+      width 
+    } = this.props
+
     const isMobile = getIsMobile(width)
+    const sections = isEmpty(activePage) 
+      ? {}
+      : activePage.sections
 
     return children({
-      isMobile
+      isMobile,
+      sections
     })
   }
 }
 
 const mapStateToProps = state => ({
   activeLocale: getActiveLocale(state),
+  activePage: getActivePage(state),
   allPages: getAllPages(state)
 })
 
